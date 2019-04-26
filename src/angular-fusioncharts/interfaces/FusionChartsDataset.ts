e171b2797c5a3800217706d40329c867843b8b79
Célian Veyssière <celian-veyssiere@harvest.fr>
2019-04-26 14:05:26 +0200

    fix typo

--- src/angular-fusioncharts/interfaces/FusionChartsDataset.ts
+++ src/angular-fusioncharts/interfaces/FusionChartsDataset.ts
@@ -1,6 +1,4 @@
-
-
-export type FusionChartsBoolean  boolean | 1 | 0 | '1' | '0' | 'true' | 'false';
+export type FusionChartsBoolean = boolean | 1 | 0 | '1' | '0' | 'true' | 'false';
 export type FusionChartsDecimal = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
 export interface FusionChartsDatasetChart {
